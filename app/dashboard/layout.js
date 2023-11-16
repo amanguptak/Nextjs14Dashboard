65d6fbb48de9d9d0b9eb24e4e933b129c7c9ed9a
Aman Gupta <eagleax7@gmail.com>
2023-11-16 14:54:45 +0530

    dashbord and login routes

---
+++ app/dashboard/layout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../components/dashboard/navbar/Navbar'
+import Sidebar from '../components/dashboard/sidebar/Sidebar'
+
+const Layout = ({children}) => {
+  return (
+    <div>
+        <div>
+        <Sidebar/>
+        </div>
+        <div>
+        <Navbar/>
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default Layout
---
+++ app/dashboard/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const dashboard = () => {
+  return (
+    <div>dashboardHi</div>
+  )
+}
+
+export default dashboard
