ff804c904722a66e97d3187f74643b8068ffb0b8
Aman Gupta <eagleax7@gmail.com>
2023-11-26 15:17:35 +0530

    model-and-db-created

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,13 +1,40 @@
-import mongoose from "mongoose"
+const mongoose = require('mongoose');
 
-export const ConnectToDb = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGODB_URI, {
-   
-        })
-      
-    }catch(err){
-        console.log(err)
+// Replace 'your_database_url' with the actual connection string for your MongoDB database.
+const dbUrl = process.env.MONGO;
+
+const connectToDatabase = async () => {
+    try {
+        // Connect to the MongoDB database
+        await mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        // Get the default connection
+        const db = mongoose.connection;
+
+        // Event listeners for connection events
+        db.on('connected', () => {
+            console.log('Mongoose connected to ' + dbUrl);
+        });
+
+        db.on('error', (err) => {
+            console.log('Mongoose connection error: ' + err);
+        });
+
+        db.on('disconnected', () => {
+            console.log('Mongoose disconnected');
+        });
+
+        // Close the Mongoose connection when the Node process is terminated
+        process.on('SIGINT', () => {
+            db.close(() => {
+                console.log('Mongoose disconnected through app termination');
+                process.exit(0);
+            });
+        });
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
     }
+};
 
-}
+// Call the asynchronous function to connect to the database
+connectToDatabase();
